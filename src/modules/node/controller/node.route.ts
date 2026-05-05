ef0e1a5a28a86a6691b2bc5ff9a42de96aa2e812
Haolong <62168247+HaolongChen@users.noreply.github.com>
2026-05-05 12:13:09 +0800

    Revert architectural-debt changes outside src/index.ts

--- src/modules/node/controller/node.route.ts
+++ src/modules/node/controller/node.route.ts
@@ -1,21 +1,15 @@
 import type { RepositoryInjectionType } from '../../../DI/repository.js';
-import { PrintTree } from '../application/printTree.js';
-import { projectAuthMiddleware } from '@/session/controller/projectAuth.middleware.js';
-import { ValidateProjectId } from '@/session/application/validate.js';
 import express from 'express';
 import { NodeController } from './node.controller.js';
+import { projectAuthMiddleware } from '@/session/controller/projectAuth.middleware.js';
 
 export const nodeRouter = (repositoryInjection: RepositoryInjectionType) => {
   const router: express.Router = express.Router();
-  const printTreeUseCase = new PrintTree(repositoryInjection.rootNodeRepository);
-  const validateProjectIdUseCase = new ValidateProjectId(
-    repositoryInjection.userRepository,
-  );
-  const nodeController = new NodeController(printTreeUseCase);
+  const nodeController = new NodeController(repositoryInjection);
 
   router.get(
     '/:projectId',
-    projectAuthMiddleware(validateProjectIdUseCase),
+    projectAuthMiddleware(repositoryInjection),
     nodeController.getTree,
   );
   return router;
