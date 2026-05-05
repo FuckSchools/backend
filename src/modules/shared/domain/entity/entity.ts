2d82e65e7b17365eec5719b8b502e6cb716dd321
Haolong <62168247+HaolongChen@users.noreply.github.com>
2026-05-05 11:48:42 +0800

    chore: freeze entity data to preserve immutability

--- src/modules/shared/domain/entity/entity.ts
+++ src/modules/shared/domain/entity/entity.ts
@@ -1,7 +1,19 @@
 import z from 'zod';
 
+function deepFreeze<T>(value: T): Readonly<T> {
+  if (value !== null && typeof value === 'object' && !Object.isFrozen(value)) {
+    Object.freeze(value);
+
+    for (const key of Object.keys(value as Record<string, unknown>)) {
+      deepFreeze((value as Record<string, unknown>)[key]);
+    }
+  }
+
+  return value as Readonly<T>;
+}
+
 export class Entity<T extends z.ZodObject = z.ZodObject> {
-  private readonly _data: z.infer<T>;
+  private _data: Readonly<z.infer<T>>;
   private _id: string;
 
   constructor(
@@ -9,16 +21,20 @@ export class Entity<T extends z.ZodObject = z.ZodObject> {
     public readonly schema: T,
     id?: string,
   ) {
-    this._data = this.schema.parse(data);
+    this._data = deepFreeze(this.schema.parse(data));
     this._id = id ? z.uuidv4().parse(id) : crypto.randomUUID();
   }
 
-  get data(): z.infer<T> {
+  get data(): Readonly<z.infer<T>> {
     return this._data;
   }
 
   get id(): string {
     return this._id;
+  }
+
+  public updateData(newData: z.infer<T>): void {
+    this._data = deepFreeze(this.schema.parse(newData));
   }
 
   protected updateId(newId: string): void {
